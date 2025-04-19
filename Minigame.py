e2f2e13f16778c3156af2b35967d8038079f2459
glep-pyto <glep320@gmail.com>
2025-04-20 00:18:27 +0800

    2nd remake, managed to add a backround img

--- Minigame.py
+++ Minigame.py
@@ -1,19 +1,21 @@
 #to import pygame into codes, along with some other modules
 import pygame
 import sys
-import tkinter as tk 
-from pygame import mixer
+import os
 
 #pygame initialisation
 pygame.init()
-mixer.init()
+
+#set fps to 60
+
+fps=60
 
 #window building, 800x600
 screen_width = 800
 screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("IDLEStudy: Beat rhythm")
-
+screen.fill((255,0,0))
 
 #set colors
 WHITE = (255, 255, 255)
@@ -28,27 +30,35 @@ BG_COLOR = (30, 30, 30)
 title_font = pygame.font.Font(None,100)
 font = pygame.font.Font(None,36)
 
-#set bg
-background_image = pygame.image.load("assets/lofi.jpg")
+#set bg if it works
+background_image = pygame.image.load("wallpep.jpg")
 background_image = pygame.transform.scale(background_image, (screen_width, screen_height))
+screen.blit(background_image,(0,0))
+#update as of 19/4,it works now
 
-#button config
-button_width = 220
-button_height = 40
-dropdown_button_width = 400
-dropdown_button_height = 40
-selectsong_button_width = 400
-selectsong_button_height = 40
+#mainmenu setup
+def main_menu():
+    menu_text = pygame.font.SysFont("Roboto", 100).render("Beat Rhythm", True, GRAY)
+    menu_rect = menu_text.get_rect(center=(400, 100))
 
-#dropdown button positions
-title_y = 20
-button_x = (screen_width - button_width) // 2
-button_y = 350
-selectsong_button_x = (screen_width - dropdown_button_width) // 2
-selectsong_button_y = 270
-dropdown_button_x = (screen_width - dropdown_button_width) // 2
-dropdown_button_y = 430
+    while True:
+        
 
-#confirmation button
-button_box = pygame.Rect(button_x, button_y, button_width, button_height)
-button_text = "Confirm and Play"
+        pos = pygame.mouse.get_pos()
+
+        screen.blit(menu_text, menu_rect)
+
+  
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+           
+                    
+                    
+#this to make sure the game proceeds and do not crash on startup
+#not sure how to not have it respond atm
+#DONT REMOVE IT
+while True:
+    pygame.display.update()
