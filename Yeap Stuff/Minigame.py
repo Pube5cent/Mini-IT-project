17fca7a6e4524c54c249c8c96a4763bbe0a41d47
glep-pyto <glep320@gmail.com>
2025-04-21 10:11:53 +0800

    still trying to figure out how to make the game respond

--- Yeap Stuff/Minigame.py
+++ Yeap Stuff/Minigame.py
@@ -15,7 +15,7 @@ screen_width = 800
 screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("IDLEStudy: Beat rhythm")
-screen.fill((255,0,0))
+
 
 #set colors
 WHITE = (255, 255, 255)
@@ -36,26 +36,7 @@ background_image = pygame.transform.scale(background_image, (screen_width, scree
 screen.blit(background_image,(0,0))
 #update as of 19/4,it works now
 
-#mainmenu setup
-def main_menu():
-    menu_text = pygame.font.SysFont("Roboto", 100).render("Beat Rhythm", True, GRAY)
-    menu_rect = menu_text.get_rect(center=(400, 100))
 
-    while True:
-        
-
-        pos = pygame.mouse.get_pos()
-
-        screen.blit(menu_text, menu_rect)
-
-  
-        
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-           
-                    
                     
 #this to make sure the game proceeds and do not crash on startup
 #not sure how to not have it respond atm
