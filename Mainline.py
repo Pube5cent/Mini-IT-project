25dd6fc90454a319be0163e6310381c589005529
Rhayyan Iman <iman.rhayyan@gmail.com>
2025-06-09 04:54:59 +0800

    idk whats wrong with the rebirth

--- Mainline.py
+++ Mainline.py
@@ -518,22 +518,15 @@ while True:
                     bonus += active_upgrades["fast_click"]["level"] * 0.5
                 Knowledge += Knowledge_per_click * bonus * Rebirth_multiplier 
         
-            if rebirth_button.collidepoint(event.pos):  # <- Ensure this is inside the first MOUSEBUTTONDOWN check
+            elif rebirth_button.collidepoint(mx, my):
                 if rebirth_system.can_rebirth(Knowledge):
-                    # Call rebirth and unpack results correctly
-                    Knowledge, Knowledge_per_click, items, active_upgrades, rebirth_multiplier, rebirth_count = rebirth_system.rebirth(
-                        Knowledge,
-                        Knowledge_per_click,
-                        items,
-                        active_upgrades
-                    )
-                    # Update rebirth_system internal states if needed
-                    rebirth_system.multiplier = rebirth_multiplier
-                    rebirth_system.rebirth_count = rebirth_count
-
-                    print(f"Rebirthed! New multiplier: {rebirth_multiplier}")
+                    Knowledge = 0
+                    rebirth_system.rebirth()
+                    Rebirth_multiplier = rebirth_system.multiplier
+                    print("Rebirth successful! Multiplier:", Rebirth_multiplier)
                 else:
-                    print("Not enough knowledge to rebirth!")
+                    print("Not enough Knowledge to rebirth. Need:", rebirth_system.cost)
+
 
 
             else:
